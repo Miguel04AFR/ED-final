cd6e0882380964f219a33ef50f45f7c09aebfa37
Miguel04AFR <miguelflorin4@gmail.com>
2025-02-09 20:14:42 -0500

    asd

--- src/interfaz/ComponenteRobot.java
+++ src/interfaz/ComponenteRobot.java
@@ -9,82 +9,100 @@ import javax.swing.JComponent;
 
 import logica.Robot;
 
-public class ComponenteRobot extends JComponent{
-	 private Robot robot;
-	 
-	 public ComponenteRobot() {
-		 robot=new Robot(60,60,50);
-			setRobot(robot);
-		}
+public class ComponenteRobot extends JComponent {
+    private Robot robot;
 
-	public ComponenteRobot(Robot robot) {
-		setRobot(robot);
-	}
+    public ComponenteRobot() {
+        robot = new Robot(60, 60, 50);
+        setRobot(robot);
+    }
 
-	public Robot getRobot() {
-		return robot;
-	}
+    public ComponenteRobot(Robot robot) {
+        setRobot(robot);
+    }
 
-	public void setRobot(Robot robot) {
-		this.robot = robot;
-	}
-	
+    public Robot getRobot() {
+        return robot;
+    }
 
-@Override
- protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    Graphics2D g2d = (Graphics2D) g; 
- // Coordenadas base 
-    int baseX = robot.getX();
-    int baseY = robot.getY() + robot.getTamano();
-    int torsoTa = robot.getTamano();
-    int brazoTa = robot.getTamano() / 5;
-    int piernaTa = robot.getTamano() / 4;
-    int rostro = robot.getTamano() / 2;
-    int ojosTa = robot.getTamano() / 5;
-    String nombre = "Precius";
-    int nombreTa = g2d.getFontMetrics().stringWidth(nombre);
-    
-    //Dibuja el nombre (Precius el tanke)
-    g2d.setColor(Color.BLACK);
-    Font originalFuente = g2d.getFont();
-    g2d.setFont(new Font("Arial", Font.BOLD, 16));
-    g2d.drawString(nombre, baseX-10 + (robot.getTamano() - nombreTa) / 2, robot.getY() - 10);
-    g2d.setFont(originalFuente);
-    
+    public void setRobot(Robot robot) {
+        this.robot = robot;
+    }
 
-    // Dibuja la cabeza del robot
-    g2d.setColor(Color.GRAY);
-    g2d.fillRect(robot.getX(), robot.getY(), robot.getTamano(), robot.getTamano());
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
 
-    // Dibuja los ojos del robot
-    g2d.setColor(Color.BLACK);
-    
-    g2d.fillRect(robot.getX() + ojosTa, robot.getY() + ojosTa, ojosTa, ojosTa);
-    g2d.fillRect(robot.getX() + 3 * ojosTa, robot.getY() + ojosTa, ojosTa, ojosTa);
+        // Coordenadas base 
+        int baseX = robot.getX();
+        int baseY = robot.getY() + robot.getTamano();
+        int torsoTa = robot.getTamano();
+        int brazoTa = robot.getTamano() / 5;
+        int piernaTa = robot.getTamano() / 4;
+        int rostro = robot.getTamano() / 2;
+        int ojosTa = robot.getTamano() / 5;
+        int orejaTa = robot.getTamano() / 4;
+        String nombre = "Freddy";
+        int nombreTa = g2d.getFontMetrics().stringWidth(nombre);
 
-    // Dibuja la boca del robot
-    g2d.setColor(Color.RED);
-    g2d.fillRect(robot.getX() + ojosTa, robot.getY() + 3 * ojosTa, rostro, ojosTa);
-    
- 
+        // Dibuja el nombre
+        g2d.setColor(Color.BLACK);
+        Font originalFuente = g2d.getFont();
+        g2d.setFont(new Font("Arial", Font.BOLD, 16));
+        g2d.drawString(nombre, baseX - 10 + (robot.getTamano() - nombreTa) / 2, robot.getY() - 20);
+        g2d.setFont(originalFuente);
 
-    // Dibuja el torso
-    g2d.setColor(Color.DARK_GRAY);
-    g2d.fillRect(baseX, baseY, robot.getTamano(), torsoTa);
+        // Dibuja la cabeza del robot
+        g2d.setColor(new Color(139, 69, 19)); // Color marrón
+        g2d.fillRect(robot.getX(), robot.getY(), robot.getTamano(), robot.getTamano());
 
-    // Dibuja los brazos
-    g2d.setColor(Color.LIGHT_GRAY);
-    g2d.fillRect(baseX - brazoTa, baseY, brazoTa, torsoTa); // Brazo izquierdo
-    g2d.fillRect(baseX + robot.getTamano(), baseY, brazoTa, torsoTa); // Brazo derecho
+        // Dibuja los ojos del robot
+        g2d.setColor(Color.WHITE);
+        g2d.fillOval(robot.getX() + ojosTa, robot.getY() + ojosTa, ojosTa, ojosTa); // Ojo derecho
+        g2d.fillOval(robot.getX() + 3 * ojosTa - 5, robot.getY() + ojosTa, ojosTa, ojosTa); // Ojo izquierdo
+        g2d.setColor(Color.BLACK);
+        g2d.fillOval(robot.getX() + 3 * ojosTa - ojosTa / 4 - 2, robot.getY() + ojosTa + ojosTa / 4, ojosTa / 2, ojosTa / 2); // Pupila ojo izquierdo
+        g2d.fillOval(robot.getX() + ojosTa + ojosTa / 4, robot.getY() + ojosTa + ojosTa / 4, ojosTa / 2, ojosTa / 2); // Pupila ojo derecho
 
-    // Dibuja las piernas
-    g2d.setColor(Color.GRAY);
-    g2d.fillRect(baseX, baseY + torsoTa, piernaTa, torsoTa); // Pierna izquierda
-    g2d.fillRect(baseX + robot.getTamano() - piernaTa, baseY + torsoTa, piernaTa, torsoTa); // Pierna derecha
+        // Dibuja la boca del robot
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(robot.getX() + ojosTa, robot.getY() + 3 * ojosTa, rostro, ojosTa);
+
+        // Dibuja las orejas
+        g2d.setColor(new Color(139, 69, 19)); // Color marrón
+        g2d.fillOval(robot.getX() - orejaTa / 2, robot.getY() + ojosTa / 2, orejaTa, orejaTa);
+        g2d.fillOval(robot.getX() + robot.getTamano() - orejaTa / 2, robot.getY() + ojosTa / 2, orejaTa, orejaTa);
+
+        // Dibuja el sombrero
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(robot.getX() + robot.getTamano() / 4, robot.getY() - robot.getTamano() / 4, robot.getTamano() / 2, robot.getTamano() / 4);
+        g2d.fillRect(robot.getX() + robot.getTamano() / 3, robot.getY() - robot.getTamano() / 3, robot.getTamano() / 3, robot.getTamano() / 6);
+
+        // Dibuja el torso
+        g2d.setColor(new Color(160, 82, 45)); // Color marrón
+        g2d.fillRect(baseX, baseY, robot.getTamano(), torsoTa);
+
+        // Dibuja los botones en el torso
+        g2d.setColor(Color.BLACK);
+        g2d.fillOval(baseX + robot.getTamano() / 2 - ojosTa / 2, baseY + torsoTa / 4, ojosTa, ojosTa);
+        g2d.fillOval(baseX + robot.getTamano() / 2 - ojosTa / 2, baseY + torsoTa / 2, ojosTa, ojosTa);
+        g2d.fillOval(baseX + robot.getTamano() / 2 - ojosTa / 2, baseY + 3 * torsoTa / 4, ojosTa, ojosTa);
+
+        // Dibuja los brazos
+        g2d.setColor(new Color(139, 69, 19)); // Color marrón
+        g2d.fillRect(baseX - brazoTa, baseY, brazoTa, torsoTa); // Brazo izquierdo
+        g2d.fillRect(baseX + robot.getTamano(), baseY, brazoTa, torsoTa); // Brazo derecho
+
+        // Dibuja las piernas
+        g2d.setColor(new Color(160, 82, 45)); // Color marrón
+        g2d.fillRect(baseX, baseY + torsoTa, piernaTa, torsoTa); // Pierna izquierda
+        g2d.fillRect(baseX + robot.getTamano() - piernaTa, baseY + torsoTa, piernaTa, torsoTa); // Pierna derecha
+
+        // Dibuja detalles adicionales en el torso
+        g2d.setColor(Color.BLACK);
+        g2d.drawRect(baseX + torsoTa / 4, baseY + torsoTa / 4, torsoTa / 2, torsoTa / 2); // Detalle en el torso
+        g2d.drawRect(baseX + torsoTa / 4, baseY + torsoTa / 4 + 13, torsoTa / 2, torsoTa / 2); // Detalle en el torso
+        g2d.drawLine(baseX + torsoTa / 4, baseY + torsoTa / 2, baseX + 3 * torsoTa / 4, baseY + torsoTa / 2); // Línea horizontal en el torso
+    }
 }
-
-	 
-}
-
-
